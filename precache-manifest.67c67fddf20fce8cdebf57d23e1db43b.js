ae8b757c4cd6d50a4154d270f761b6a63c83b4f2
utkarsh0911 <65824700+utkarsh0911@users.noreply.github.com>
2020-09-18 05:52:47 -0700

    Updates

---
+++ precache-manifest.67c67fddf20fce8cdebf57d23e1db43b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f373cb6a80b13003312f7d892167a5",
+    "url": "/my_web_resume/index.html"
+  },
+  {
+    "revision": "7217d540ea8c1d9b8b8b",
+    "url": "/my_web_resume/static/css/main.34cb0936.chunk.css"
+  },
+  {
+    "revision": "cd2bbd3822323938557a",
+    "url": "/my_web_resume/static/js/2.71cc7d99.chunk.js"
+  },
+  {
+    "revision": "3ae53551bb61526a017fad7619b64b1b",
+    "url": "/my_web_resume/static/js/2.71cc7d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7217d540ea8c1d9b8b8b",
+    "url": "/my_web_resume/static/js/main.d05336c8.chunk.js"
+  },
+  {
+    "revision": "bffebd5a34e9d31bfb08",
+    "url": "/my_web_resume/static/js/runtime-main.fcb9f4b0.js"
+  },
+  {
+    "revision": "bfcbffefc60ba15c0f9112e1fc27b46b",
+    "url": "/my_web_resume/static/media/edu.bfcbffef.jpeg"
+  },
+  {
+    "revision": "9f188c03c175d4deb5f37d8017583511",
+    "url": "/my_web_resume/static/media/expbg.9f188c03.jpeg"
+  },
+  {
+    "revision": "c4dded400c21f1d4f16c38e944cb582b",
+    "url": "/my_web_resume/static/media/experience.c4dded40.png"
+  },
+  {
+    "revision": "4bf1f2ec323d887f04522469695815c7",
+    "url": "/my_web_resume/static/media/landingBackground.4bf1f2ec.jpg"
+  },
+  {
+    "revision": "4249c8f234c3b41719f964ab60689af2",
+    "url": "/my_web_resume/static/media/logo.4249c8f2.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/my_web_resume/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "673050149fc3aa634239142996f86bc6",
+    "url": "/my_web_resume/static/media/mapmyindia.67305014.jpg"
+  },
+  {
+    "revision": "243af5462fe68c01da91b6339b343e49",
+    "url": "/my_web_resume/static/media/photo.243af546.png"
+  },
+  {
+    "revision": "9708b0f348985d3f4fa2b6721e2c3e57",
+    "url": "/my_web_resume/static/media/resume.9708b0f3.pdf"
+  },
+  {
+    "revision": "93912274e22637e7a9502f53fc331c2b",
+    "url": "/my_web_resume/static/media/skill.93912274.jpeg"
+  },
+  {
+    "revision": "cab517d95ccbb804db3db38f63636b6c",
+    "url": "/my_web_resume/static/media/tcs.cab517d9.jpg"
+  }
+]);
